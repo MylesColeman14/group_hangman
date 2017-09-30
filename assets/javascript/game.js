08945dac9a0acb26a6eefbe388222d3216fc4f6c
addison0043 <addison.albrecht@yahoo.com>
2017-09-30 13:48:01 -0500

    Update game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,12 @@
  	var letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
- 	var hallow ["SUPERNATURAL", "COBWEB", "BROOMSTICK", "HOWL", "SKELETONS", "NOOSE", "GRAVE", "CREEP", "SCREAM", "CANDY APPLES"];
+ 	var hallow = ["SUPERNATURAL", "COBWEB", "BROOMSTICK", "HOWL", "SKELETONS", "NOOSE", "GRAVE", "CREEP", "SCREAM", "CANDY APPLES"];
  	//do we ant this global? we can also put it in pickRandomWord()
- 	var guessed =[] //array for guesses letters
+ 	var guessed =[]; //array for guessed letters
 
     var guesses_left = 10;
     var word;
     var guess;
-    var words;
+    var randomWord;
     var count;
     var space; // var for spaces
 
@@ -16,17 +16,45 @@
     var losses = 0;
     $("#defeat").text(losses);*/
 
-    function pickRandomWord(){ 
-    var randomNum = Math.floor(Math.random()*hallow.length);
-    randomWord = hallow[randomNum].toLowerCase();
-    console.log(“The word is: ” + randomWord);
-	}
 
 
-    // //Checks if keyopress
-    // document.onkeyup = function(event){
-    //     if(event.keyCode <= 90 && event.keyCode >= 65 && wrongLetter.includes(event.key) == false){
-    //         wordSearch(event.key); // this fucntion can be replaced with whatever we need
-    //     }
-    // }
-    // //===========================
+    function keypress() {
+    	guess = event.key.toUpperCase();
+    	console.log(guess);
+    		// set attribute
+    	for (var i = 0; i < randomWord; i++) {
+ 			if (randomWord[i] === guess) {
+ 				$("#word_to_guess").text(guess);
+ 			}    
+
+        }
+
+    }
+
+    function result() {
+    	for (var i = 0; i < randomWord.length; i++) {
+ 			if (randomWord[i] === "-") {
+ 				$("#word_to_guess").append("&nbsp");
+ 				space += 1;
+ 			}
+ 			else {
+ 				$("#word_to_guess").append("_ ");
+ 			}
+        }
+        
+    }
+
+    //Checks if keyopress
+    document.onkeyup = function(event){
+        
+        randomWord = hallow[Math.floor(Math.random()*hallow.length)];
+        randomWord = randomWord.split("");
+        console.log(randomWord);
+
+        word = randomWord;
+
+        	result();
+ 			keypress();   
+ 			 
+    }
+

76f6c5bf31ff9ebcd5ffac655ec9352d0b2ee0bc
Myles Coleman <myles.coleman14@gmail.com>
2017-09-30 11:42:16 -0500

    added the new function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,6 @@
  	var letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+ 	var hallow ["SUPERNATURAL", "COBWEB", "BROOMSTICK", "HOWL", "SKELETONS", "NOOSE", "GRAVE", "CREEP", "SCREAM", "CANDY APPLES"];
+ 	//do we ant this global? we can also put it in pickRandomWord()
  	var guessed =[] //array for guesses letters
 
     var guesses_left = 10;
@@ -8,11 +10,14 @@
     var count;
     var space; // var for spaces
 
-
-
-
-    $("#guesses").text(guesses_left);
+    /*$("#guesses").text(guesses_left);
     var wins = 0;
     $("#victory").text(wins);
     var losses = 0;
-    $("#defeat").text(losses);
+    $("#defeat").text(losses);*/
+
+    function pickRandomWord(){ 
+    var randomNum = Math.floor(Math.random()*hallow.length);
+    randomWord = hallow[randomNum].toLowerCase();
+    console.log(“The word is: ” + randomWord);
+	}
